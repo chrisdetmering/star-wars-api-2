985407b6b37a2fe6b552bb3dd2cf757eef7199f4
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-02-19 14:22:50 -0600

    Remove API call from file, import table.css

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,29 +1,10 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
-import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-const getData = async (speciesURL) => {
-  let data;
-  try {
-    data = await axios.get(speciesURL);
-  } catch (error) {
-    console.log('Error: ', error);
-  }
-  console.log(data);
-  return data;
-};
+import '../styles/table.css';
 
 function displayData(props) {
-  console.log(props.swBulkData);
-  let species;
   return props.swBulkData.map((characterInfo) => {
-    if (characterInfo.species.length === 0) {
-      species = 'Human';
-    } else {
-      const { data } = getData(characterInfo.species);
-      species = data.name;
-    }
     return (
       <tr>
         <td>{characterInfo.name}</td>
@@ -31,7 +12,7 @@ function displayData(props) {
         <td>{characterInfo.height}</td>
         <td>{characterInfo.mass}</td>
         <td>{characterInfo.homeworld}</td>
-        <td>{species}</td>
+        <td>{characterInfo.species}</td>
       </tr>
     );
   });
@@ -40,7 +21,7 @@ function displayData(props) {
 function Table(props) {
   return (
     <div>
-      <table className="table">
+      <table id="data-table" className="table">
         <thead>
           <tr>
             <th>Name</th>
